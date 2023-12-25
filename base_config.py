fa8b4399802d7dc97b8899ccd61d1bc8caa3d8c7
jianing <lijn2003@protonmail.com>
2023-12-25 16:40:36 +0800

    Base configuration file

---
+++ base_config.py
@@ -0,0 +1,30 @@
+# Base configuration file - rename to "config.py"
+
+# API
+TOKEN = 'MAPILLARY-API-TOKEN'
+ASYNC = True
+
+# Coords
+MIN_LAT = 1.237
+MAX_LAT = 1.473
+MIN_LNG = 103.605
+MAX_LNG = 104.042
+BBOX_SIZE = 0.001
+BBOX_NUM_IMG = 30
+COORD_ACC = 3
+
+# Files
+IMG_DIR = 'img/'
+LABELS_FILE = 'img/labels.csv'
+MODEL_DIR = 'models/'
+
+# Data
+IMG_SIZE = 576 # shortest edge
+TRAIN_BATCH_SIZE = 64
+TEST_BATCH_SIZE = 128
+SHUFFLE = True
+PIN_MEMORY = True
+DROP_LAST = True
+
+# Model
+EPOCHS = 1
