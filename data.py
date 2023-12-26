0abf01591e0118307638397259595a205bb52d67
jianing <lijn2003@protonmail.com>
2023-12-26 17:44:14 +0800

    Test-loading data

--- data.py
+++ data.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import time
 
 import torch
+# from torch import multiprocessing
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision.transforms import v2
 from torchvision.io import read_image
@@ -29,7 +31,7 @@ class ImageDataset(Dataset): # ImageFolder???
         try:
             img = read_image(img_path)
         except Exception as e:
-            raise Exception(f'Unsupported img file: {img_path}')
+            raise Exception(img_path)
 
         labels = self.labels.iloc[idx, 1:]
         labels = np.array(labels, dtype=float)
@@ -40,12 +42,15 @@ class ImageDataset(Dataset): # ImageFolder???
         return img, labels
 
 
-def scale_targets(labels):
-    lat = labels[0]
-    lng = labels[1]
-    y1 = (lat-config.MIN_LAT) / (config.MAX_LAT-config.MIN_LAT)
-    y2 = (lng-config.MIN_LNG) / (config.MAX_LNG-config.MIN_LNG)
-    return torch.tensor([y1, y2])
+# def scale_targets(labels):
+#     lat = labels[0]
+#     lng = labels[1]
+#     y1 = (lat-config.MIN_LAT) / (config.MAX_LAT-config.MIN_LAT)
+#     y2 = (lng-config.MIN_LNG) / (config.MAX_LNG-config.MIN_LNG)
+#     return torch.tensor([y1, y2])
+
+def linear_scale(x):
+    return torch.as_tensor(x-x.min()) / (x.max()-x.min())
 
 
 def make_dataloaders():
@@ -60,7 +65,7 @@ def make_dataloaders():
     ])
 
     target_transform = v2.Compose([
-            scale_targets,
+            linear_scale,
             v2.ToDtype(torch.float32),
     ])
 
@@ -109,14 +114,30 @@ def make_dataloaders():
 
 
 if __name__ == '__main__':
+    start = time.time()
+    # multiprocessing.set_start_method('spawn')
+
     train_dataloader, val_dataloader, test_dataloader = make_dataloaders()
+    # for batch, (X, y) in enumerate(train_dataloader):
+    #     print(f'Batch: {batch}')
+    #     print(f'X: {X.shape}')
+    #     print(f'y: {y.shape}')
+
+    #     img = X[0]
+    #     plt.imshow(img.permute(1, 2, 0))
+    #     plt.show()
+
+    #     break
+    print('Loading train data...')
     for batch, (X, y) in enumerate(train_dataloader):
-        print(f'Batch: {batch}')
-        print(f'X: {X.shape}')
-        print(f'y: {y.shape}')
-
-        img = X[0]
-        plt.imshow(img.permute(1, 2, 0))
-        plt.show()
-
-        break
+        if not batch % 100:
+            print(f'Train n_batch@{time.time()-start}: {batch}')
+    print('Loading val data...')
+    for batch, (X, y) in enumerate(val_dataloader):
+        if not batch % 100:
+            print(f'Val n_batch@{time.time()-start}: {batch}')
+    print('Loading test data...')
+    for batch, (X, y) in enumerate(test_dataloader):
+        if not batch % 100:
+            print(f'Test n_batch@{time.time()-start}: {batch}')
+    print('Data loading passed')
