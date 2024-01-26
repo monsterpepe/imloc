f9d4f2fec040c5778b8fcf67ce5afec16e003454
jianing <lijn2003@protonmail.com>
2024-01-26 12:02:44 +0800

    __name__ == '__main__'

--- num_params.py
+++ num_params.py
@@ -1,9 +1,10 @@
 import numpy as np
-from net import Net
 
 def num_params(model):
     params = filter(lambda p: p.requires_grad, model.parameters())
     return sum([np.prod(p.size()) for p in params])
 
-model = Net()
-print(num_params(model))
+if __name__ == '__main__':
+    from net import Net
+    model = Net()
+    print(num_params(model))
